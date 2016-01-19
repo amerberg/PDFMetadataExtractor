2a9277de22125f84d3dd9d31f8f20189558d867f
Adam Merberg <amerberg@gmail.com>
2016-01-18 19:48:56 -0500

    Fix pattern building and add label search

--- py/find_label.py
+++ py/find_label.py
@@ -1,9 +1,9 @@
+from re import search
 
 def build_character_pattern(character, substitutions):
-    substitutions.append(character)
-    substitutions = [str(c) for c in substitutions]
+    strings = [str(c) for c in (substitutions + [character])]
     return "(?:" + "|".join([s if len(s)== 1 else "(?:" + s + ")"
-                     for s in substitutions]) + ")"
+                     for s in strings]) + ")"
 
 def build_label_pattern(label, substitutions):
     return "".join([c if c not in substitutions
@@ -11,5 +11,17 @@ def build_label_pattern(label, substitutions):
                     for c in label])
 
 def build_field_pattern(field, substitutions):
+    labels = field['labels'] + [label.upper() for label in field['labels']]
     return "|".join(["(?:" + build_label_pattern(label, substitutions) + ")"
-                     for label in field['labels']])
+                     for label in labels])
+
+def find_field_label(field, document, page, substitutions):
+    pattern = build_field_pattern(field, substitutions)
+    lines = [line for line in document.lines if line.page==page]
+    result = []
+    for line in lines:
+        if search(pattern, line.text):
+            result.append(line)
+            # include positional information!
+
+    return result
