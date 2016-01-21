cdd90a1cc6df49acc3a9238ad37d0c3af0b54c83
Adam Merberg <amerberg@gmail.com>
2016-01-21 17:00:44 -0500

    Return dates as date objects

--- py/field_types.py
+++ py/field_types.py
@@ -1,5 +1,6 @@
 from re import match
 from sqlalchemy import Integer, String, Float, Boolean, Date
+from datetime import date
 
 # TODO: make everything in this file less awkward!!!!
 def col_type(field):
@@ -14,10 +15,16 @@ def col_type(field):
 def post_process(field, value):
     if field['type'] == 'date':
         result = match(r'([01]?\d)[/1-]([0-3]?\d)[/1-](\d{4})', value)
-        return "%s/%s/%s" % (result.group(3), result.group(1), result.group(2))
+        try:
+            return date(int(result.group(3)), int(result.group(1)), int(result.group(2)))
+        except ValueError:
+            return None
     elif field['type'] == 'human_name':
-        result = match('([A-Za-z01\-\s]+),\s+([A-Za-z01\-\s]+)', value)
-        name = "%s %s" % (result.group(2), result.group(1))
-        return name.title()
+        result = match(r'([A-Za-z01\-\s]+),\s*([A-Za-z01\-\s]+)', value)
+        try:
+            name = "%s %s" % (result.group(2), result.group(1))
+            return name.title()
+        except AttributeError:
+            return value.title()
     elif field['type'] == 'proper_noun':
         return value.title()
